d9abe26e6b8b6525b89e1088c4e77a7e7192baa6
ying_123 <zhangying@aten-optics.com>
2023-02-17 17:34:34 +0800

    update url parser

--- src/pages/music/index.tsx
+++ src/pages/music/index.tsx
@@ -256,7 +256,7 @@ async function parseMusicUrl(id: number): Promise<string | null> {
         ).then(
             response => response.text()
         ).then(
-            text => text.match(/https?:\/\/((|[\w-]+\.)+[a-z0-9]+)(?:(\/[^/?#]+)*)?(\?[^#]+)?(#.+)?$/i)[0]
+            text => text.match(/https?:\/\/[^<]+/i)[0]
         )
         if (url) {
             return url;
