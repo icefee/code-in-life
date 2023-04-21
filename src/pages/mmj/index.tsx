fb6286876b554935a7598682192f9b29f4d3cbf1
icefee <icefee@outlook.com>
2023-04-21 22:24:38 +0800

    update static page.

--- src/pages/mmj/index.tsx
+++ src/pages/mmj/index.tsx
@@ -5,11 +5,8 @@ import GlobalStyles from '@mui/material/GlobalStyles';
 export function Head() {
     return (
         <>
-            <meta charSet="UTF-8" />
             <link rel="icon" type="image/svg+xml" href="/mmj/vite.svg" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="stylesheet" type="text/css" href="/mmj/assets/index-cadd242c.css" />
-            <script type="module" src="/mmj/assets/index-8afe445e.js"></script>
             <title>音乐搜索</title>
         </>
     )
@@ -18,6 +15,7 @@ export function Head() {
 function Page() {
     return (
         <NoSsr>
+            <script type="module" src="/mmj/assets/index-8afe445e.js"></script>
             <GlobalStyles
                 styles={
                     `
