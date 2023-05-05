b2aa9e6d4245b31e74e37925e86016a43642db75
ying_123 <zhangying@aten-optics.com>
2023-05-05 13:35:29 +0800

    remove unused import.

--- src/util/config.ts
+++ src/util/config.ts
@@ -3,5 +3,4 @@ import { isDev } from './env'
 export abstract class Api {
     private static localhost = '127.0.0.1'
     public static hosting = isDev ? `http://${Api.localhost}:8000` : 'https://music.gatsbyjs.io'
-    public static music = 'https://www.gequbao.com'
 }
--- src/util/pipe.ts
+++
@@ -1,6 +0,0 @@
-import type { GatsbyFunctionResponse } from 'gatsby';
-
-export function setCommonHeaders(response: GatsbyFunctionResponse) {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    return response;
-}
