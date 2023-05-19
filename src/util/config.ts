abd510acd368ffafefd3747ebda98f08bc39b606
ying_123 <zhangying@aten-optics.com>
2023-05-19 17:00:41 +0800

    add api poster prefix

--- src/util/config.ts
+++ src/util/config.ts
@@ -3,4 +3,5 @@ import { isDev } from './env'
 export abstract class Api {
     private static localhost = '127.0.0.1'
     public static hosting = isDev ? `http://${Api.localhost}:8000` : 'https://music.gatsbyjs.io'
+    public static posterApiPrefix = isDev ? '' : 'https://apps.gatsbyjs.io'
 }
