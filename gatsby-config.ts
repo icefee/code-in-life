429fe0119b5c96978ad62359848862fc39b499a3
ying_123 <zhangying@aten-optics.com>
2023-07-13 11:42:26 +0800

    add pwa support

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,20 @@
 import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
-  plugins: []
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: '音乐搜索',
+        short_name: '音乐搜索',
+        icon: 'static/icon.png',
+        start_url: '/',
+        background_color: '#03a9f4',
+        theme_color: '#3f51b5',
+        display: 'standalone'
+      }
+    }
+  ]
 }
 
 export default config;
