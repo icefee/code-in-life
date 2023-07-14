c30a39306b64d07f83e8beba13d0edcfc245ead3
ying_123 <zhangying@aten-optics.com>
2023-07-14 15:20:10 +0800

    remove test code

--- src/api/music/lrc/download/[id].ts
+++ src/api/music/lrc/download/[id].ts
@@ -2,29 +2,25 @@ import { createApiAdaptor, parseId, getResponse } from '../../../../adaptors'
 import { appendContentDisposition, errorHandler, ApiHandler } from '../../../../util/middleware'
 
 const handler: ApiHandler = async (req, res) => {
-    // const { key, id } = parseId(req.params.id)
-    res.json({
-        params: JSON.stringify(req.params),
-        query: JSON.stringify(req.query)
-    })
-    // const adaptor = createApiAdaptor(key)
-    // if (adaptor.lrcFile) {
-    //     const response = await getResponse(adaptor.getLrcUrl(id))
-    //     const headers = response.headers
-    //     for (const key of headers.keys()) {
-    //         res.setHeader(key, headers.get(key))
-    //     }
-    //     response.body.pipe(appendContentDisposition(req, res, 'lrc'))
-    // }
-    // else {
-    //     const lrcText = await adaptor.getLrcText(id)
-    //     if (lrcText) {
-    //         appendContentDisposition(req, res, 'lrc').setHeader('Content-Type', 'text/lrc').send(lrcText)
-    //     }
-    //     else {
-    //         throw new Error('lrc file not found.')
-    //     }
-    // }
+    const { key, id } = parseId(req.params.id)
+    const adaptor = createApiAdaptor(key)
+    if (adaptor.lrcFile) {
+        const response = await getResponse(adaptor.getLrcUrl(id))
+        const headers = response.headers
+        for (const key of headers.keys()) {
+            res.setHeader(key, headers.get(key))
+        }
+        response.body.pipe(appendContentDisposition(req, res, 'lrc'))
+    }
+    else {
+        const lrcText = await adaptor.getLrcText(id)
+        if (lrcText) {
+            appendContentDisposition(req, res, 'lrc').setHeader('Content-Type', 'text/lrc').send(lrcText)
+        }
+        else {
+            throw new Error('lrc file not found.')
+        }
+    }
 }
 
 export default errorHandler(handler)
