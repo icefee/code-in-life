d997a4920907a3ae784ed27e85f5e269353d7f97
ying_123 <zhangying@aten-optics.com>
2023-08-01 08:44:40 +0800

    remove unused pages

--- src/api/video/proxy/parse.ts
+++
@@ -1,6 +0,0 @@
-import { errorHandler, ApiHandler, proxyJson } from '../../../util/middleware'
-import { Api } from '../../../util/config'
-
-const handler: ApiHandler = (req, res) => proxyJson(`${Api.site}/api/video/proxy/parse?${new URLSearchParams(req.query)}`, res)
-
-export default errorHandler(handler)
