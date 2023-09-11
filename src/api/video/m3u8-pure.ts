13083a52db5a76f53811a868adfa16c057cee34e
ying_123 <zhangying@aten-optics.com>
2023-09-11 13:37:47 +0800

    update video pure rule

--- src/api/video/m3u8-pure.ts
+++ src/api/video/m3u8-pure.ts
@@ -1,12 +1,27 @@
 import { getResponse } from '../../adaptors'
 import { errorHandler, ApiHandler } from '../../util/middleware'
+import { proxyUrl } from '../../util/proxy'
 
 const handler: ApiHandler = async (req, res) => {
     const { url } = req.query
     try {
-        const response = await getResponse(url)
+        const response = await getResponse(
+            proxyUrl(url, true)
+        )
         const content = await response.text()
-        const pured = content.replace(/mixed\.m3u8/, 'index.m3u8')
+        const lines = content.split(/\n/)
+        const pured = lines.map(
+            (line) => {
+                if (line.match(/.+\/mixed\.m3u8$/)) {
+                    const puredLine = line.replace(/mixed\.m3u8/, 'index.m3u8')
+                    if (puredLine.startsWith('http')) {
+                        return puredLine
+                    }
+                    return new URL(puredLine, url).href
+                }
+                return line
+            }
+        ).join('\n')
         response.headers.forEach(
             (header, key) => {
                 res.setHeader(key, header)
