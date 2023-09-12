eeef149d211cc2048f8ee8b977b74b2370545d8f
ying_123 <zhangying@aten-optics.com>
2023-09-12 17:17:21 +0800

    add adds remove

--- src/api/video/m3u8-pure.ts
+++ src/api/video/m3u8-pure.ts
@@ -1,39 +1,36 @@
-import { getResponse } from '../../adaptors'
+import { getTextWithTimeout, getResponse } from '../../adaptors'
 import { errorHandler, ApiHandler } from '../../util/middleware'
-import { proxyUrl } from '../../util/proxy'
+import { proxyUrl, removeAds } from '../../util/proxy'
 
 const handler: ApiHandler = async (req, res) => {
     const { url } = req.query
     try {
-        const response = await getResponse(
+        const source = await getTextWithTimeout(
             proxyUrl(url, true)
         )
-        const content = await response.text()
-        if (content.match(/#EXT-X-STREAM-INF/)) {
-            const lines = content.split(/\n/)
-            const pured = lines.map(
-                (line) => {
-                    if (line.match(/\.m3u8$/)) {
-                        let puredLine = line
-                        if (line.match(/.+\/mixed\.m3u8$/)) {
-                            puredLine = line.replace(/mixed\.m3u8/g, 'index.m3u8')
-                        }
-                        if (puredLine.startsWith('http')) {
-                            return puredLine
-                        }
-                        return new URL(puredLine, url).href
-                    }
-                    return line
-                }
-            ).join('\n')
-            response.headers.forEach(
-                (header, key) => {
-                    res.setHeader(key, header)
-                }
+        if (source.match(/.+\/mixed\.m3u8$/m)) {
+            const lines = source.split(/\n/)
+            const line = lines.find(
+                line => line.match(/.+\/mixed\.m3u8$/)
             )
-            const buffer = Buffer.from(pured)
-            res.setHeader('content-length', buffer.byteLength)
-            res.end(pured)
+            if (line) {
+                const puredLine = line.replace(/mixed\.m3u8/g, 'index.m3u8')
+                const playList = puredLine.startsWith('http') ? puredLine : new URL(puredLine, url).href
+                const response = await getResponse(playList)
+                response.headers.forEach(
+                    (header, key) => {
+                        res.setHeader(key, header)
+                    }
+                )
+                const content = await response.text()
+                const pured = removeAds(content, '01o')
+                const buffer = Buffer.from(pured)
+                res.setHeader('content-length', buffer.byteLength)
+                res.end(pured)
+            }
+            else {
+                throw new Error('no mixed file matched')
+            }
         }
         else {
             throw new Error('no need to pure required')
--- src/util/proxy.ts
+++ src/util/proxy.ts
@@ -11,3 +11,25 @@ export function proxyUrl(url: string, remote: boolean = false, extend = {}) {
         ...extend
     })
 }
+
+export function removeAds(content: string, mark: string) {
+    const discontinuityTag = /#EXT-X-DISCONTINUITY/
+    const infReg = new RegExp('#EXTINF:\\d+(.\\d+)?,')
+    const lines = content.split(/\n/)
+    const parts = []
+    let extLine = false
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i]
+        if (line.match(discontinuityTag)) {
+            continue;
+        }
+        if (extLine && line.indexOf(mark) !== -1) {
+            parts.pop()
+        }
+        else {
+            parts.push(line)
+        }
+        extLine = Boolean(line.match(infReg))
+    }
+    return parts.join('\n')
+}
