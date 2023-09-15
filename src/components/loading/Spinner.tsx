b2fd57e0fbadd3c0e10ae9d678cbeb41e44e63c1
ying_123 <zhangying@aten-optics.com>
2023-09-15 13:06:11 +0800

    update spinner style

--- src/components/loading/Spinner.tsx
+++ src/components/loading/Spinner.tsx
@@ -7,11 +7,6 @@ function Spinner(props: SvgIconProps) {
         <SvgIcon {...props}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <defs>
-                    <radialGradient spreadMethod="reflect">
-                        <stop offset="0%" stopColor="#03a9f4" />
-                        <stop offset="45%" stopColor="#ffc107" />
-                        <stop offset="90%" stopColor="#e92929" />
-                    </radialGradient>
                     <linearGradient id={`line-gradient-${id}`} x1="0" y1="0" x2="1" y2="1">
                         <stop offset="0%" stopColor="#03a9f4" />
                         <stop offset="100%" stopColor="#e92929" />
