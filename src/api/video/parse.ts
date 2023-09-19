158008415f3a5065cee0c96cfe32302bcaa48774
ying_123 <zhangying@aten-optics.com>
2023-09-19 13:37:34 +0800

    update url parse api

--- src/api/video/parse.ts
+++ src/api/video/parse.ts
@@ -1,6 +1,49 @@
-import { errorHandler, ApiHandler, proxyJson } from '../../util/middleware'
-import { Api } from '../../util/config'
+import { getTextWithTimeout } from '../../adaptors'
+import { errorHandler, ApiHandler } from '../../util/middleware'
+import { proxyUrl } from '../../util/proxy'
+import { isDev } from '../../util/env'
 
-const handler: ApiHandler = async (req, res) => proxyJson(`${Api.assetSite}/api/video/parse?${new URLSearchParams(req.query)}`, res)
+async function parseVideoUrl(url: string) {
+    try {
+        const html = await getTextWithTimeout(
+            isDev ? url : proxyUrl(url, true)
+        )
+        const matches = html.match(
+            new RegExp('(https?://)?[\\w\\u4e00-\\u9fa5-./@%?:]+?\\.(m3u8|webm|mp4)', 'i')
+        )
+        if (matches) {
+            const matched = matches[0]
+            if (matched.startsWith('http')) {
+                return matched
+            }
+            return new URL(matched, url).href
+        }
+        else {
+            throw new Error('😥 not matched.')
+        }
+    }
+    catch (err) {
+        return null
+    }
+}
+
+const handler: ApiHandler = async (req, res) => {
+    const { url } = req.query
+    const data = await parseVideoUrl(url)
+    if (data) {
+        res.json({
+            code: 0,
+            data,
+            msg: '成功'
+        })
+    }
+    else {
+        res.json({
+            code: -1,
+            data,
+            msg: '失败'
+        })
+    }
+}
 
 export default errorHandler(handler)
