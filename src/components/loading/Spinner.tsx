08acd02d83719b4736a6ee257ae3f7a8d46dbd84
ying_123 <zhangying@aten-optics.com>
2023-10-13 16:48:32 +0800

    update spinner style

--- src/components/loading/Spinner.tsx
+++ src/components/loading/Spinner.tsx
@@ -12,12 +12,23 @@ function Spinner(props: SvgIconProps) {
                         <stop offset="100%" stopColor="#e92929" />
                     </linearGradient>
                 </defs>
-                <path
-                    fill={`url(#line-gradient-${id})`}
-                    d="M2,12A11.2,11.2,0,0,1,13,1.05C12.67,1,12.34,1,12,1a11,11,0,0,0,0,22c.34,0,.67,0,1-.05C6,23,2,17.74,2,12Z">
-                    <animateTransform attributeName="transform" dur="0.6s" repeatCount="indefinite" type="rotate"
-                        values="0 12 12;360 12 12" />
-                </path>
+                <g
+                    fill="none"
+                    stroke={`url(#line-gradient-${id})`}
+                    strokeLinecap="round"
+                    strokeWidth="1.2">
+                    <path
+                        strokeDasharray="60"
+                        strokeDashoffset="60"
+                        strokeOpacity=".3"
+                        d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z">
+                        <animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0" />
+                    </path>
+                    <path strokeDasharray="15" strokeDashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12">
+                        <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0" />
+                        <animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12" />
+                    </path>
+                </g>
             </svg>
         </SvgIcon>
     )
