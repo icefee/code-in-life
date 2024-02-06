68159c0f86f71e5d692e2cbf2af5a9033074ecd4
icefee <icefee@outlook.com>
2024-02-06 13:20:11 +0800

    update lrc apis

--- src/adaptors/zz123.ts
+++ src/adaptors/zz123.ts
@@ -7,8 +7,6 @@ export const key = 'z';
 
 export const baseUrl = 'https://zz123.com';
 
-export const lrcFile = false;
-
 export async function getMusicSearch(s: string): Promise<SearchMusic[]> {
     const searchParams = new URLSearchParams({
         key: s
