be5cda362c44690963b33461365cfa53d434516e
ying_123 <zhangying@aten-optics.com>
2024-02-19 11:01:23 +0800

    update package version

--- src/components/player/MusicLrc.tsx
+++ src/components/player/MusicLrc.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useMemo, useRef } from 'react'
+import GlobalStyles from '@mui/material/GlobalStyles'
 import Fade from '@mui/material/Fade'
 import Popover from '@mui/material/Popover'
 import Box from '@mui/material/Box'
@@ -92,11 +93,20 @@ function MusicLrc({ id, currentTime }: MusicLrcProps) {
     }, [downloading, lineActive, lrc, lineIndex])
 
     const lineDuration = useMemo(() => {
+        let duration = 1;
         if (lineActive && lineIndex < lrc.length - 1) {
-            return lrc[lineIndex + 1].time - lrc[lineIndex].time
+            duration = lrc[lineIndex + 1].time - lrc[lineIndex].time
         }
-        return 1
+        return Math.min(duration, 3)
     }, [lineActive, lrc, lineIndex])
+
+    const linePlayedDuration = useMemo(() => {
+        let timeStart = 0
+        if (lineIndex > -1 && lrc.length > 0) {
+            timeStart = lrc[lineIndex].time
+        }
+        return Math.min(currentTime - timeStart, 3)
+    }, [lrc, lineIndex, currentTime])
 
     const placeholder = (text: string) => (
         <Box sx={{
@@ -126,13 +136,24 @@ function MusicLrc({ id, currentTime }: MusicLrcProps) {
                 direction="row"
                 justifyContent="flex-end"
             >
-                <Typography
-                    variant="caption"
-                    noWrap
-                    sx={{
-                        display: 'block',
-                        cursor: 'pointer'
-                    }}
+                <GlobalStyles
+                    styles={
+                        `
+                        @property --line-played {
+                            syntax: "<number>";
+                            inherits: true;
+                            initial-value: 0;
+                        }
+                        `
+                    }
+                />
+                <div
+                    style={{
+                        position: 'relative',
+                        cursor: 'pointer',
+                        overflow: 'hidden',
+                        '--line-played': linePlayedDuration / lineDuration
+                    } as React.CSSProperties}
                     onClick={
                         (event: React.MouseEvent<HTMLDivElement>) => {
                             setAnchorEl(event.currentTarget);
@@ -140,28 +161,52 @@ function MusicLrc({ id, currentTime }: MusicLrcProps) {
                     }
                     title={displayLrc}
                 >
-                    {
-                        displayLrc.split('').map(
-                            (text, index, chars) => {
-                                const totalDelay = Math.min(lineDuration, 3) * 1000
-                                const delay = (totalDelay / chars.length / 2) * index
-                                return (
-                                    <Fade
-                                        key={lineIndex + '-' + index}
-                                        in={show}
-                                        timeout={Math.min(totalDelay / 2, 800)}
-                                        style={{
-                                            transitionDelay: delay + 'ms'
-                                        }}
-                                        unmountOnExit
-                                    >
-                                        <span>{text}</span>
-                                    </Fade>
-                                )
-                            }
-                        )
-                    }
-                </Typography>
+                    <Typography
+                        variant="caption"
+                        sx={{
+                            display: 'block',
+                            opacity: .75,
+                            transition: 'all .4s linear',
+                            maskImage: 'linear-gradient(0.25turn, transparent 0% calc(var(--line-played) * 100%), #000 calc(var(--line-played) * 100%) 100%)'
+                        }}
+                        noWrap
+                    >
+                        {
+                            displayLrc.split('').map(
+                                (text, index, chars) => {
+                                    const lineDurationMilliseconds = lineDuration * 1e3
+                                    const delay = (lineDurationMilliseconds / chars.length) * index
+                                    return (
+                                        <Fade
+                                            key={lineIndex + '-' + index}
+                                            in={show}
+                                            timeout={Math.min(lineDurationMilliseconds / 2, 800)}
+                                            style={{
+                                                transitionDelay: delay / 4 + 'ms'
+                                            }}
+                                            unmountOnExit
+                                        >
+                                            <span>{text}</span>
+                                        </Fade>
+                                    )
+                                }
+                            )
+                        }
+                    </Typography>
+                    <Typography
+                        variant="caption"
+                        sx={{
+                            position: 'absolute',
+                            inset: 0,
+                            transition: 'all .4s linear',
+                            backgroundImage: 'linear-gradient(0, #ff9800, cyan)',
+                            WebkitBackgroundClip: 'text',
+                            WebkitTextFillColor: 'transparent',
+                            maskImage: 'linear-gradient(0.25turn, #000 0% calc(var(--line-played) * 100%), transparent calc(var(--line-played) * 100%) 100%)'
+                        }}
+                        noWrap
+                    >{displayLrc}</Typography>
+                </div>
             </Stack>
             <Popover
                 open={Boolean(anchorEl)}
@@ -214,47 +259,52 @@ function ScrollingLrc({ lrc, currentTime }: ScrollingLrcProps) {
     }, [lrc, currentTime])
 
     return (
-        <Box sx={(theme) => ({
-            height: '40vh',
-            maxHeight: 400,
-            minWidth: 240,
-            maxWidth: 'var(--max-width)',
-            [theme.breakpoints.only('xs')]: {
-                maxWidth: '75vw'
-            }
-        })}>
-            <Box sx={{
-                position: 'relative',
-                height: '100%',
-                overflow: 'hidden',
-                px: 2,
-                py: 1,
-                maskImage: 'linear-gradient(transparent, rgb(0 0 0 / 15%) 20%, #000 40% 60%, rgb(0 0 0 / 15%) 80%, transparent)'
-            }}>
-                <Box sx={{
-                    transition: (theme) => theme.transitions.create('transform'),
-                    transform: `translate(0, calc(20vh - 24px - ${28 * activeIndex}px))`
-                }}>
+        <Box
+            sx={(theme) => ({
+                height: '40vh',
+                maxHeight: 400,
+                minWidth: 240,
+                maxWidth: 'var(--max-width)',
+                [theme.breakpoints.only('xs')]: {
+                    maxWidth: '75vw'
+                }
+            })}
+        >
+            <Box
+                sx={{
+                    height: '100%',
+                    overflow: 'hidden',
+                    px: 2,
+                    py: 1,
+                    maskImage: 'linear-gradient(transparent, rgb(0 0 0 / 15%) 20%, #000 40% 60%, rgb(0 0 0 / 15%) 80%, transparent)'
+                }}
+            >
+                <Box
+                    sx={{
+                        transition: (theme) => theme.transitions.create('transform'),
+                        transform: `translate(0, calc(20vh - 24px - ${28 * activeIndex}px))`
+                    }}
+                >
                     {
                         lrc.map(
-                            ({ text }, index) => {
-                                const isActive = activeIndex === index;
-                                return (
-                                    <Stack sx={{
+                            ({ text }, index) => (
+                                <Stack
+                                    sx={{
                                         height: 28,
-                                        color: isActive ? 'secondary.main' : 'text.primary'
-                                    }} justifyContent="center" key={index}>
-                                        <Typography
-                                            variant="subtitle2"
-                                            color="inherit"
-                                            textOverflow="ellipsis"
-                                            textAlign="center"
-                                            title={text}
-                                            noWrap
-                                        >{text}</Typography>
-                                    </Stack>
-                                )
-                            }
+                                        color: activeIndex === index ? 'secondary.main' : 'text.primary'
+                                    }}
+                                    justifyContent="center"
+                                    key={index}
+                                >
+                                    <Typography
+                                        variant="subtitle2"
+                                        color="inherit"
+                                        textAlign="center"
+                                        title={text}
+                                        noWrap
+                                    >{text}</Typography>
+                                </Stack>
+                            )
                         )
                     }
                 </Box>
