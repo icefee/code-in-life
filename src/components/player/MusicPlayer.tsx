c605dd9fee07309b869f002ab5b25f48f19b0ebb
ying_123 <zhangying@aten-optics.com>
2024-02-27 13:56:44 +0800

    update player

--- src/components/player/MusicPlayer.tsx
+++ src/components/player/MusicPlayer.tsx
@@ -27,7 +27,7 @@ export enum RepeatMode {
 }
 
 interface MusicPlayerProps {
-    music?: SearchMusic;
+    music: SearchMusic;
     playing: boolean;
     repeat: RepeatMode;
     extendButtons: React.ReactNode;
@@ -71,7 +71,7 @@ function MusicPlayer({
                 hasError.current = false
             }
         }
-    }, [music?.url])
+    }, [music.url])
 
     const togglePlay = async (play: boolean) => {
         try {
@@ -167,37 +167,33 @@ function MusicPlayer({
                     alignItems="center"
                     flexShrink={0}
                 >
-                    {
-                        music && (
-                            <Box
-                                sx={(theme) => ({
-                                    width: '100%',
-                                    height: '100%',
-                                    bgcolor: '#000',
-                                    border: '6px solid hsla(0, 0%, 100%, .04)',
-                                    p: .75,
-                                    borderRadius: '50%',
-                                    filter: playing ? `drop-shadow(0px 0px 16px ${alpha(theme.palette.secondary.main, .4)})` : 'none',
-                                    opacity: loading ? .5 : .8,
-                                    transition: theme.transitions.create(['opacity', 'filter'])
-                                })}
-                            >
-                                <Box
-                                    sx={{
-                                        height: '100%',
-                                        border: '1px solid #333',
-                                        borderRadius: '50%'
-                                    }}
-                                >
-                                    <MusicPoster
-                                        alt={`${music.name}-${music.artist}`}
-                                        src={music.poster}
-                                        spinning={playing && !loading}
-                                    />
-                                </Box>
-                            </Box>
-                        )
-                    }
+                    <Box
+                        sx={(theme) => ({
+                            width: '100%',
+                            height: '100%',
+                            bgcolor: '#000',
+                            border: '6px solid hsla(0, 0%, 100%, .04)',
+                            p: .75,
+                            borderRadius: '50%',
+                            filter: playing ? `drop-shadow(0px 0px 16px ${alpha(theme.palette.secondary.main, .4)})` : 'none',
+                            opacity: loading ? .5 : .8,
+                            transition: theme.transitions.create(['opacity', 'filter'])
+                        })}
+                    >
+                        <Box
+                            sx={{
+                                height: '100%',
+                                border: '1px solid #333',
+                                borderRadius: '50%'
+                            }}
+                        >
+                            <MusicPoster
+                                alt={`${music.name}-${music.artist}`}
+                                src={music.poster}
+                                spinning={playing && !loading}
+                            />
+                        </Box>
+                    </Box>
                     <Box
                         sx={{
                             position: 'absolute',
@@ -253,32 +249,28 @@ function MusicPlayer({
                         >
                             <Typography
                                 variant="body2"
-                                title={music?.name}
+                                title={music.name}
                                 noWrap
-                            >{music?.name}</Typography>
+                            >{music.name}</Typography>
                         </Stack>
                         <Stack>
                             <Typography
                                 variant="caption"
                                 color="#ffffffcc"
-                                title={music?.artist}
+                                title={music.artist}
                                 lineHeight={1}
                                 noWrap
-                            >{music?.artist}</Typography>
+                            >{music.artist}</Typography>
                         </Stack>
-                        {
-                            music && (
-                                <Box
-                                    sx={{
-                                        flexGrow: 1,
-                                        alignSelf: 'flex-start',
-                                        overflow: 'hidden'
-                                    }}
-                                >
-                                    <MusicLrc id={music.id} currentTime={currentTime} />
-                                </Box>
-                            )
-                        }
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                alignSelf: 'flex-start',
+                                overflow: 'hidden'
+                            }}
+                        >
+                            <MusicLrc id={music.id} currentTime={currentTime} />
+                        </Box>
                     </Stack>
                     <Stack
                         direction="row"
@@ -373,109 +365,104 @@ function MusicPlayer({
                         </Tooltip>
                     </Stack>
                 </Stack>
-                {
-                    music && (
-                        <audio
-                            ref={audioRef}
-                            preload="auto"
-                            onLoadStart={
-                                () => setLoading(true)
+                <audio
+                    ref={audioRef}
+                    preload="auto"
+                    onLoadStart={
+                        () => setLoading(true)
+                    }
+                    onDurationChange={
+                        (event: React.SyntheticEvent<HTMLVideoElement>) => {
+                            setDuration(event.currentTarget.duration);
+                        }
+                    }
+                    onLoadedMetadata={
+                        () => {
+                            setAudioReady(true);
+                            tryToAutoPlay();
+                        }
+                    }
+                    onCanPlay={
+                        () => {
+                            setLoading(false)
+                        }
+                    }
+                    onCanPlayThrough={
+                        () => {
+                            setLoading(false)
+                        }
+                    }
+                    onPlay={
+                        () => {
+                            onPlayStateChange(true)
+                        }
+                    }
+                    onPause={
+                        () => {
+                            onPlayStateChange(false)
+                        }
+                    }
+                    onWaiting={
+                        () => {
+                            setLoading(true)
+                        }
+                    }
+                    onTimeUpdate={
+                        () => {
+                            if (!seekingRef.current) {
+                                setCurrentTime(audioRef.current.currentTime)
                             }
-                            onDurationChange={
-                                (event: React.SyntheticEvent<HTMLVideoElement>) => {
-                                    setDuration(event.currentTarget.duration);
+                        }
+                    }
+                    onProgress={
+                        () => {
+                            if (audioReady) {
+                                const buffered = audioRef.current.buffered;
+                                let bufferedEnd: number;
+                                try {
+                                    bufferedEnd = buffered.end(buffered.length - 1);
                                 }
-                            }
-                            onLoadedMetadata={
-                                () => {
-                                    setAudioReady(true);
-                                    tryToAutoPlay();
+                                catch (err) {
+                                    bufferedEnd = 0;
                                 }
+                                setBuffered(bufferedEnd / duration)
                             }
-                            onCanPlay={
-                                () => {
-                                    setLoading(false)
-                                }
+                        }
+                    }
+                    onSeeked={
+                        () => {
+                            seekingRef.current = false
+                        }
+                    }
+                    onEnded={
+                        () => {
+                            audioRef.current.currentTime = 0
+                            if (repeat === RepeatMode.One) {
+                                tryToAutoPlay()
                             }
-                            onCanPlayThrough={
-                                () => {
-                                    setLoading(false)
-                                }
+                            else {
+                                onPlayEnd?.(true)
                             }
-                            onPlay={
-                                () => {
-                                    onPlayStateChange(true)
-                                }
+                        }
+                    }
+                    onVolumeChange={
+                        () => setVolume(audioRef.current.volume)
+                    }
+                    onError={
+                        () => {
+                            if (hasError.current) {
+                                onPlayEnd?.(false)
+                                setLoading(false)
+                                onPlayStateChange(false)
                             }
-                            onPause={
-                                () => {
-                                    onPlayStateChange(false)
-                                }
+                            else {
+                                hasError.current = true
+                                setTimeout(reloadSong, 400)
                             }
-                            onWaiting={
-                                () => {
-                                    setLoading(true)
-                                }
-                            }
-                            onTimeUpdate={
-                                () => {
-                                    if (!seekingRef.current) {
-                                        setCurrentTime(audioRef.current.currentTime)
-                                    }
-                                }
-                            }
-                            onProgress={
-                                () => {
-                                    if (audioReady) {
-                                        const buffered = audioRef.current.buffered;
-                                        let bufferedEnd: number;
-                                        try {
-                                            bufferedEnd = buffered.end(buffered.length - 1);
-                                        }
-                                        catch (err) {
-                                            bufferedEnd = 0;
-                                        }
-                                        setBuffered(bufferedEnd / duration)
-                                    }
-                                }
-                            }
-                            onSeeked={
-                                () => {
-                                    seekingRef.current = false
-                                }
-                            }
-                            onEnded={
-                                () => {
-                                    audioRef.current.currentTime = 0
-                                    if (repeat === RepeatMode.One) {
-                                        tryToAutoPlay()
-                                    }
-                                    else {
-                                        onPlayEnd?.(true)
-                                    }
-                                }
-                            }
-                            onVolumeChange={
-                                () => setVolume(audioRef.current.volume)
-                            }
-                            onError={
-                                () => {
-                                    // audioRef.current.src = music.url;
-                                    if (hasError.current) {
-                                        onPlayEnd?.(false)
-                                        setLoading(false)
-                                        onPlayStateChange(false)
-                                    }
-                                    else {
-                                        hasError.current = true
-                                        setTimeout(reloadSong, 400)
-                                    }
-                                }
-                            }
-                            src={music.url}
-                        />
-                    )
-                }
+                        }
+                    }
+                    src={music.url}
+                />
             </Stack>
             {
                 isDev && (
