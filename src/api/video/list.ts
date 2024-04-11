00cce9ca4bf5f13777e8c7c2a6e7186cc03a5020
icefee <icefee@outlook.com>
2024-04-11 17:22:57 +0800

    update api

--- src/api/video/list.ts
+++ src/api/video/list.ts
@@ -2,7 +2,7 @@ import { errorHandler, ApiHandler, proxyJson } from '../../util/middleware'
 import { Api } from '../../util/config'
 
 const handler: ApiHandler = (req, res) => {
-    return proxyJson(`${Api.site}/api/video/list` + req.url.replace(/^\//, ''), res)
+    return proxyJson(Api.site + req.url, res)
 }
 
 export default errorHandler(handler)
