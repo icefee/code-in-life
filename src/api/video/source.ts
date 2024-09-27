24f716c43d0ca94574c91608b4c97bef9fc9795a
icefee <icefee@outlook.com>
2024-09-27 17:17:04 +0800

    update api -> source

--- src/api/video/source.ts
+++ src/api/video/source.ts
@@ -3,14 +3,18 @@ import { Middleware, Config, getJson } from '../../adaptors'
 type DataSource = {
     key: string;
     rating: number;
-    group: 'normal' | '18+'
+    searchDisable?: boolean;
+    group: string;
 }
 
 const handler: Middleware.ApiHandler = async (_, res) => {
     const { data } = await getJson<ApiJsonType<DataSource[]>>(`${Config.Api.site}/api/video/source`)
     const keys = []
     const preferKeys = []
-    for (const { key, rating, group } of data) {
+    for (const { key, rating, group, searchDisable } of data) {
+        if (searchDisable) {
+            continue
+        }
         const k = `${key}_${rating}`
         if (group === 'normal') {
             keys.push(k)
